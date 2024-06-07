c699798c9b20f7acba37a6b4923f17b51d7fb158
littlemiracler <lilu.murmansk@yandex.ru>
2024-06-07 16:29:39 +0300

    Add files via upload

---
+++ module3hard.py
@@ -0,0 +1,32 @@
+print('Дополнительное практическое задание по модулю №3')
+print("________________________________________________")
+
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+def calculate_structure_sum(list_):
+    total_sum = 0
+    if isinstance(list_, (int, float)):
+        return list_
+    elif isinstance(list_, str):
+        return len(list_)
+    elif isinstance(list_, (list, tuple, set)):
+        for item in list_:
+            total_sum += calculate_structure_sum(item)
+    elif isinstance(list_, dict):
+        for key, value in list_.items():
+            total_sum += calculate_structure_sum(key)
+            total_sum += calculate_structure_sum(value)
+    return total_sum
+
+
+result = calculate_structure_sum(data_structure)
+print(result)
+print("________________________________________________")
+print('Дополнительное практическое задание по модулю №3: выполнено')
